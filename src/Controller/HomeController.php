31aec95049cd9b453f12e416aa9290d84809e577
Emeline Tranois <emeline.tranois@laposte.net>
2018-08-03 10:47:31 +0200

    dev: finish template signin and signup

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,15 +12,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            "user" => [
-                "nom" => "Bob",
-                "prenom" => "Patrick",
-                "avatar" => "https://i.skyrock.net/1024/28511024/pics/2123012031_1.jpg",
-                // Si l'image avait été sur notre pc il faut donc ecrir le chemin "asset/static/images/500.png", mais pour le placer
-                // ensuite il faudra donc ecrire {{ absolute_url (asset (user.avatar)) }} pour quand on voudra le placer dans le code html
-            ]
-        ]);
+        // return $this->render('home/index.html.twig', [
+        //     'controller_name' => 'HomeController',
+        //     "user" => [
+        //         "nom" => "Bob",
+        //         "prenom" => "Patrick",
+        //         "avatar" => "https://i.skyrock.net/1024/28511024/pics/2123012031_1.jpg",
+        //         // Si l'image avait été sur notre pc il faut donc ecrir le chemin "asset/static/images/500.png", mais pour le placer
+        //         // ensuite il faudra donc ecrire {{ absolute_url (asset (user.avatar)) }} pour quand on voudra le placer dans le code html
+        //     ]
+        // ]);
+        return $this->render('offline/signup.html.twig');
     }
 }
